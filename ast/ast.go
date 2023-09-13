510193328d1720cef7405041c5bacc936ae6e9c6
wnvd <waninaveed21@gmail.com>
2023-09-13 16:24:53 +0530

    add Abstract Syntax Tree basic code, create parser file in pkg

---
+++ ast/ast.go
@@ -0,0 +1,48 @@
+package ast
+
+import (
+	"github.com/monkey/token"
+)
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statemens []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statemens) > 0 {
+		return p.Statemens[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()  {}
+func (ls *LetStatement) expressionNode() {}
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
