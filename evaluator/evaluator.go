e262c04b1c1de80cb4a4b73b2e2849f280d4f80e
wnvd <waninaveed21@gmail.com>
2023-10-04 16:18:06 +0530

    evaluating expression/integer literals

---
+++ evaluator/evaluator.go
@@ -0,0 +1,32 @@
+package evaluator
+
+import (
+	"github.com/monkey/ast"
+	"github.com/monkey/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	// Statements
+	case *ast.Program:
+		return evalStatements(node.Statements)
+
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	//Expression
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statements := range stmts {
+		result = Eval(statements)
+	}
+
+	return result
+}
