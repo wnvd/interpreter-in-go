05b39a5b73b3fbaf087b630f90d8344bea43256d
wnvd <waninaveed21@gmail.com>
2023-10-08 20:51:12 +0530

    added string concatenation

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -149,6 +149,8 @@ func evalInfixExpression(
 		return nativeBoolToBooleanObject(left == right)
 	case operator == "!=":
 		return nativeBoolToBooleanObject(left != right)
+	case left.Type() == object.STRING_OBJ && right.Type() == object.STRING_OBJ:
+		return evalStringInfixExpression(operator, left, right)
 	case left.Type() != right.Type():
 		return newError("type mismatch: %s %s %s", left.Type(), operator, right.Type())
 	default:
@@ -298,4 +300,16 @@ func unwrapReturnValue(obj object.Object) object.Object {
 		return returnValue.Value
 	}
 	return obj
+}
+
+func evalStringInfixExpression(
+	operator string,
+	left, right object.Object,
+) object.Object {
+	if operator != "+" {
+		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
+	}
+	leftVal := left.(*object.String).Value
+	rightVal := right.(*object.String).Value
+	return &object.String{Value: leftVal + rightVal}
 }
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -224,9 +224,13 @@ func TestErrorHandling(t *testing.T) {
 			"unknown operator: BOOLEAN + BOOLEAN",
 		},
 		{
+			`"Hello" + " " + "World!"`,
+			"unkown operator: STRING - STRING",
+		},
+		{
 			"foobar",
 			"indentifier not found: foobar",
-		},
+        },
 	}
 
 	for _, tt := range tests {
@@ -325,5 +329,18 @@ func TestStringLiteral(t *testing.T) {
 	}
 	if str.Value != "Hello World!" {
 		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value, got=%q", str.Value)
 	}
 }
